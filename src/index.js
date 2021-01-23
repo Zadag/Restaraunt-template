6bd4bb3985d8da94996294e2d6c289c50d0fb112
Kevin <kbrowne97@gmail.com>
2021-01-23 07:25:42 -0500

    adds about.js imports about to index

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
+import { app } from './app'
+import { about } from './about'
+//const content = document.querySelector('#content');
 
-const content = document.querySelector('#content');
+app.makeNav();
+about();
 
-const makeNav = (() => {
-    const nav = document.createElement('div');
-    nav.id = 'nav-bar';
-    content.appendChild(nav);
-
-    const leftNav = document.createElement('div');
-    leftNav.id = 'left-nav';
-    nav.appendChild(leftNav);
-
-    const logo = document.createElement('img');
-    logo.src = 'imgs/clipart1382579.png';
-    logo.id = 'ant-img';
-    leftNav.appendChild(logo);
-
-    const rightNav = document.createElement('div');
-    rightNav.id = 'right-nav';
-    nav.appendChild(rightNav);
-
-    const about = document.createElement('button');
-    about.classList.add('nav-button');
-    about.textContent = 'About';
-    rightNav.appendChild(about);
-
-    const menu = document.createElement('button');
-    menu.classList.add('nav-button');
-    menu.textContent = 'Menu';
-    rightNav.appendChild(menu);
-
-    const contact = document.createElement('button');
-    contact.classList.add('nav-button');
-    contact.textContent = 'Contact';
-    rightNav.appendChild(contact);
-})();
