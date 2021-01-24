b3b8ccc5f4788e2d4ea538aedc340cd435d7a169
Kevin <kbrowne97@gmail.com>
2021-01-24 13:33:13 -0500

    adds contact module adds tab switching logic to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 import { app } from './app'
 import { about } from './about'
 import { menu } from './menu'
+import { contact } from './contact'
+
 const content = document.querySelector('#content');
 
 app.makeNav();
+about();
 
-menu();
+const changeTab = (e) => {
+    app.clear();
+    app.makeNav();
+    listeners();
+    switch(e.target.textContent) {
+        case 'About':
+            about();
+            break;
+        case 'Menu':
+            menu();
+            break;
+        case 'Contact':
+            contact();
+            break;
+    }
+}
+
+const listeners = () => {
+    const btns = document.querySelectorAll('button');
+
+    btns.forEach(button => {
+        button.addEventListener('click', changeTab);
+    })    
+}
+
+listeners();
+
+
+
+
+
 
